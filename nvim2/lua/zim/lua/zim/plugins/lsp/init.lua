0dcd89c9be587b4ff5b86c4358f97edb6ef3f7e8
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-09-14 11:41:47 -0500

    more lsp and lang tool stuff

--- nvim2/lua/zim/lua/zim/plugins/lsp/init.lua
+++ nvim2/lua/zim/lua/zim/plugins/lsp/init.lua
@@ -48,6 +48,49 @@ return {
 				},
 				-- lsp server settings
 				servers = {
+					-- docker
+					dockerls = {},
+					-- docker-compose
+					docker_compose_language_service = {},
+					-- go
+					gopls = {
+						settings = {
+							gopls = {
+								gofumpt = true,
+								codelenses = {
+									gc_details = false,
+									generate = true,
+									regenerate_cgo = true,
+									run_govulncheck = true,
+									test = true,
+									tidy = true,
+									upgrade_dependency = true,
+									vendor = true,
+								},
+								hints = {
+									assignVariableTypes = true,
+									compositeLiteralFields = true,
+									compositeLiteralTypes = true,
+									constantValues = true,
+									functionTypeParameters = true,
+									parameterNames = true,
+									rangeVariableTypes = true,
+								},
+								analyses = {
+									nilness = true,
+									unusedparams = true,
+									unusedwrite = true,
+									useany = true,
+								},
+								usePlaceholders = true,
+								completeUnimported = true,
+								staticcheck = true,
+								directoryFilters = { "-.git", "-.vscode", "-.idea", "-.vscode-test", "-node_modules" },
+								semanticTokens = true,
+							},
+						},
+					},
+					-- lua
 					lua_ls = {
 						settings = {
 							Lua = {
@@ -74,8 +117,235 @@ return {
 							},
 						},
 					},
+					-- markdown
+					marksman = {},
+					-- prisma
+					prismals = {},
+					-- python
+					basedpyright = {},
+					ruff = {},
+					-- tailwind
+					tailwindcss = {
+						filetypes_exclude = { "markdown" },
+					},
+					-- terraform
+					terraformls = {},
+					-- typescript
+					vtsls = {
+						-- explicitly add default filetypes, so that we can extend
+						-- them in related extras
+						filetypes = {
+							"javascript",
+							"javascriptreact",
+							"javascript.jsx",
+							"typescript",
+							"typescriptreact",
+							"typescript.tsx",
+						},
+						settings = {
+							complete_function_calls = true,
+							vtsls = {
+								enableMoveToFileCodeAction = true,
+								autoUseWorkspaceTsdk = true,
+								experimental = {
+									maxInlayHintLength = 30,
+									completion = {
+										enableServerSideFuzzyMatch = true,
+									},
+								},
+							},
+							typescript = {
+								updateImportsOnFileMove = { enabled = "always" },
+								suggest = {
+									completeFunctionCalls = true,
+								},
+								inlayHints = {
+									enumMemberValues = { enabled = true },
+									functionLikeReturnTypes = { enabled = true },
+									parameterNames = { enabled = "literals" },
+									parameterTypes = { enabled = true },
+									propertyDeclarationTypes = { enabled = true },
+									variableTypes = { enabled = false },
+								},
+							},
+						},
+						keys = {
+							{
+								"gD",
+								function()
+									local params = vim.lsp.util.make_position_params()
+									Zim.lsp.execute({
+										command = "typescript.goToSourceDefinition",
+										arguments = { params.textDocument.uri, params.position },
+										open = true,
+									})
+								end,
+								desc = "Goto Source Definition",
+							},
+							{
+								"gR",
+								function()
+									Zim.lsp.execute({
+										command = "typescript.findAllFileReferences",
+										arguments = { vim.uri_from_bufnr(0) },
+										open = true,
+									})
+								end,
+								desc = "File References",
+							},
+							{
+								"<leader>co",
+								Zim.lsp.action["source.organizeImports"],
+								desc = "Organize Imports",
+							},
+							{
+								"<leader>cM",
+								Zim.lsp.action["source.addMissingImports.ts"],
+								desc = "Add missing imports",
+							},
+							{
+								"<leader>cu",
+								Zim.lsp.action["source.removeUnused.ts"],
+								desc = "Remove unused imports",
+							},
+							{
+								"<leader>cD",
+								Zim.lsp.action["source.fixAll.ts"],
+								desc = "Fix all diagnostics",
+							},
+							{
+								"<leader>cV",
+								function()
+									Zim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
+								end,
+								desc = "Select TS workspace version",
+							},
+						},
+					},
+					-- yaml
+					yamlls = {
+						-- have to add this for yamlls to understand that we support line folding
+						capabilities = {
+							textDocument = {
+								foldingRange = {
+									dynamicRegistration = false,
+									lineFoldingOnly = true,
+								},
+							},
+						},
+						-- lazy-load schemastore when needed
+						-- on_new_config = function(new_config)
+						-- 	new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+						-- 		"force",
+						-- 		new_config.settings.yaml.schemas or {},
+						-- 		require("schemastore").yaml.schemas()
+						-- 	)
+						-- end,
+						settings = {
+							redhat = { telemetry = { enabled = false } },
+							yaml = {
+								keyOrdering = false,
+								format = {
+									enable = true,
+								},
+								validate = true,
+								schemaStore = {
+									-- disable in favor or schemastore.nvim
+									enable = false,
+									-- avoid typeerror: cannot read properties of undefined (reading 'length')
+									url = "",
+								},
+							},
+						},
+					},
 				},
-				setup = {},
+				setup = {
+					gopls = function(_, _)
+						Zim.lsp.on_attach(function(client, _)
+							if not client.server_capabilities.semanticTokensProvider then
+								local semantic = client.config.capabilities.textDocument.semanticTokens
+								client.server_capabilities.semanticTokensProvider = {
+									full = true,
+									legend = {
+										tokenTypes = semantic.tokenTypes,
+										tokenModifiers = semantic.tokenModifiers,
+									},
+									range = true,
+								}
+							end
+						end, "gopls")
+					end,
+					ruff = function()
+						Zim.lsp.on_attach(function(client, _)
+							client.server_capabilities.hoverProvider = false
+						end)
+					end,
+					tailwindcss = function(_, opts)
+						local tw = Zim.lsp.get_raw_config("tailwindcss")
+						opts.filetypes = opts.filetypes or {}
+						-- add default file types
+						vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+						-- exclude user file types
+						opts.filetypes = vim.tbl_filter(function(ft)
+							return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+						end, opts.filetypes)
+					end,
+					vtsls = function(_, opts)
+						Zim.lsp.on_attach(function(client, buffer)
+							client.commands["_typescript.moveToFileRefactoring"] = function(command, ctx)
+								---@type string, string, lsp.Range
+								local action, uri, range = unpack(command.arguments)
+
+								local function move(newf)
+									client.request("workspace/executeCommand", {
+										command = command.command,
+										arguments = { action, uri, range, newf },
+									})
+								end
+
+								local fname = vim.uri_to_fname(uri)
+								client.request("workspace/executeCommand", {
+									command = "typescript.tsserverRequest",
+									arguments = {
+										"getMoveToRefactoringFileSuggestions",
+										{
+											file = fname,
+											startLine = range.start.line + 1,
+											startOffset = range.start.character + 1,
+											endLine = range["end"].line + 1,
+											endOffset = range["end"].character + 1,
+										},
+									},
+								}, function(_, result)
+									---@type string[]
+									local files = result.body.files
+									table.insert(files, 1, "Enter new path...")
+									vim.ui.select(files, {
+										prompt = "Select move destination:",
+										format_item = function(f)
+											return vim.fn.fnamemodify(f, ":~:.")
+										end,
+									}, function(f)
+										if f and f:find("^Enter new path") then
+											vim.ui.input({
+												prompt = "Enter move destination:",
+												default = vim.fn.fnamemodify(fname, ":h") .. "/",
+												completion = "file",
+											}, function(newf)
+												return newf and move(newf)
+											end)
+										elseif f then
+											move(f)
+										end
+									end)
+								end)
+							end
+						end, "vtsls")
+						-- copy typescript settings to javascript
+						opts.settings.javascript =
+							vim.tbl_deep_extend("force", {}, opts.settings.typescript, opts.settings.javascript or {})
+					end,
+				},
 			}
 			return ret
 		end,
---
+++ scripts/install-language-tools.sh
@@ -0,0 +1,198 @@
+#!/usr/bin/env bash
+
+# ensure homebrew installed
+if ![[ command -v brew ]] &>/dev/null; then
+  echo "🚨 homebrew is required to run this script. please install it."
+  exit 1
+fi
+
+# ensure go installed
+if ![[ command -v go ]] &>/dev/null; then
+  echo "🚨 go >= 1.25 is required to run this script. please install it."
+  exit 1
+fi
+
+# check if go >= 1.25
+goversion="$(go version | awk '{print $3}' | sed 's/go//')"
+if [[ "$goversion" != 1.25.* ]]; then
+  echo "🚨 go >= 1.25 is required to run this script. please install it."
+  exit 1
+fi
+
+# ensure npm installed
+if ![[ command -v npm ]] &>/dev/null; then
+  echo "🚨 node >= 24 is required to run this script. please install it."
+  exit 1
+fi
+
+# check if node >= 24
+nodeversion="$(node --version | sed 's/v//')"
+if [[ "$nodeversion" != 24.* ]]; then
+  echo "🚨 node >= 24 is required to run this script. please install it."
+  exit 1
+fi
+
+# docker
+echo "📦 installing docker tools"
+brew install dockerfile-language-server &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed dockerfile-language-server"
+else
+  echo "  🚨 failed to install dockerfile-language-server"
+fi
+brew install hadolint &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed hadolint"
+else
+  echo "  🚨 failed to install hadolint"
+fi
+
+# docker compose
+echo "📦 installing docker-compose tools"
+brew install docker-compose-langserver &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed docker-compose-langserver"
+else
+  echo "  🚨 failed to install docker-compose-langserver"
+fi
+
+# go
+echo "📦 installing golang tools"
+go install golang.org/x/tools/gopls@latest &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed gopls"
+else
+  echo "  🚨 failed to install gopls"
+fi
+go install golang.org/x/tools/cmd/goimports@latest &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed goimports"
+else
+  echo "  🚨 failed to install goimports"
+fi
+go install github.com/segmentio/golines@latest &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed golines"
+else
+  echo "  🚨 failed to golines"
+fi
+go install mvdan.cc/gofumpt@latest &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed gofumpt"
+else
+  echo "  🚨 failed to install gofumpt"
+fi
+go install github.com/securego/gosec/v2/cmd/gosec@latest &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed gosec"
+else
+  echo "  🚨 failed to install gosec"
+fi
+go install honnef.co/go/tools/cmd/staticcheck@latest &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed staticcheck"
+else
+  echo "  🚨 failed to install staticcheck"
+fi
+go install github.com/mgechev/revive@latest &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed revive"
+else
+  echo "  🚨 failed to install revive"
+fi
+
+# markdown
+echo "📦 installing markdown tools"
+brew install marksman &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed marksman"
+else
+  echo "  🚨 failed to install marksman"
+fi
+brew install markdownlint-cli2 &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed markdownlint-cli2"
+else
+  echo "  🚨 failed to install markdownlint-cli2"
+fi
+brew install markdown-toc &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed markdown-toc"
+else
+  echo "  🚨 failed to install markdown-toc"
+fi
+
+# prisma
+echo "📦 installing prisma tools"
+npm i -g @prisma/language-server &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed prisma-language-server"
+else
+  echo "  🚨 failed to install prisma-language-server"
+fi
+
+# python
+echo "📦 installing python tools"
+brew install basedpyright &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed basedpyright"
+else
+  echo "  🚨 failed to install basedpyright"
+fi
+brew install ruff &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed ruff"
+else
+  echo "  🚨 failed to install ruff"
+fi
+
+# sh
+echo "📦 installing sh tools"
+brew install shfmt &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed shfmt"
+else
+  echo "  🚨 failed to install shfmt"
+fi
+
+# sql
+echo "📦 installing sql tools"
+brew install sqlfluff &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed sqlfluff"
+else
+  echo "  🚨 failed to install sqlfluff"
+fi
+
+# tailwind
+echo "📦 installing tailwind tools"
+brew install tailwindcss-language-server &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed tailwindcss-language-server"
+else
+  echo "  🚨 failed to install tailwindcss-language-server"
+fi
+
+# terraform
+echo "📦 installing terraform tools"
+brew install hashicorp/tap/terraform-ls &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed terraform-ls"
+else
+  echo "  🚨 failed to install terraform-ls"
+fi
+brew install tflint &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed tflint"
+else
+  echo "  🚨 failed to install tflint"
+fi
+
+# yaml
+echo "📦 installing yaml tools"
+brew install yaml-language-server &>/dev/null
+if [[ $? -eq 0 ]]; then
+  echo "  ✅ installed yaml-language-server"
+else
+  echo "  🚨 failed to install yaml-language-server"
+fi
