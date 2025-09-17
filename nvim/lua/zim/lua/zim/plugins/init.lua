eb1f27283824f077c0c42aba02a619b45878b06d
Andrew J. McGehee <andrewjmcgehee.us@gmail.com>
2025-09-17 16:29:09 -0500

    trying to add snacks

--- nvim/lua/zim/lua/zim/plugins/init.lua
+++ nvim/lua/zim/lua/zim/plugins/init.lua
@@ -18,15 +18,25 @@ return {
 		"folke/snacks.nvim",
 		priority = 1000,
 		lazy = false,
-		opts = {},
-		config = function(_, opts)
-			local notify = vim.notify
-			require("snacks").setup(opts)
-			-- HACK: restore vim.notify after snacks setup and let noice.nvim take over
-			-- this is needed to have early notifications show up in noice history
-			if Zim.has("noice.nvim") then
-				vim.notify = notify
-			end
-		end,
+		opts = { explorer = { enabled = true } },
+		keys = {
+			{
+				"<leader>e", function() Snacks.explorer({ cwd = LazyVim.root() }) end,
+				desc = "Explorer Root",
+			},
+			{
+				"<leader>E", function() Snacks.explorer() end,
+				desc = "Explorer CWD",
+			},
+		},
+		-- config = function(_, opts)
+		-- 	local notify = vim.notify
+		-- 	require("snacks").setup(opts)
+		-- 	-- HACK: restore vim.notify after snacks setup and let noice.nvim take over
+		-- 	-- this is needed to have early notifications show up in noice history
+		-- 	if Zim.has("noice.nvim") then
+		-- 		vim.notify = notify
+		-- 	end
+		-- end,
 	},
 }
